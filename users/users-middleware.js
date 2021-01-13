29a8a7c0251b6c3239f6bcf267c012f50844fde1
Jason Maurer <jsonmaur@fastmail.com>
2021-01-12 19:41:39 -0700

    Code from guided lecture

--- users/users-middleware.js
+++ users/users-middleware.js
@@ -1,11 +1,20 @@
+const bcrypt = require("bcryptjs")
+const Users = require("./users-model")
+
 function restrict() {
 	// This middleware function should restrict routes to authorized users only.
 	// It should get the username and password from the request headers.
 	return async (req, res, next) => {
-		try {
-			const { username, password } = req.headers
+		const authError = {
+			message: "Invalid credentials",
+		}
 
-			
+		try {
+			if (!req.session || !req.session.user) {
+				return res.status(401).json(authError)
+			}
+
+			next()
 		} catch (err) {
 			next(err)
 		}
