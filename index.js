69ded00350c3c82574e014ce47fc6a8ecfc255e2
Jason Maurer <jsonmaur@fastmail.com>
2021-03-09 13:08:57 -0700

    Update dependencies

--- index.js
+++ index.js
@@ -1,24 +1,6 @@
-const express = require("express")
-const helmet = require("helmet")
-const cors = require("cors")
-const usersRouter = require("./users/users-router")
+const server = require("./server")
 
-const server = express()
 const port = process.env.PORT || 5000
-
-server.use(helmet())
-server.use(cors())
-server.use(express.json())
-
-server.use(usersRouter)
-
-server.use((err, req, res, next) => {
-	console.log(err)
-	
-	res.status(500).json({
-		message: "Something went wrong",
-	})
-})
 
 server.listen(port, () => {
 	console.log(`Running at http://localhost:${port}`)
--- users/users-middleware.js
+++ users/users-middleware.js
@@ -1,11 +1,7 @@
 function restrict() {
-	// This middleware function should restrict routes to authorized users only.
-	// It should get the username and password from the request headers.
 	return async (req, res, next) => {
 		try {
-			const { username, password } = req.headers
-
-			
+			next()
 		} catch (err) {
 			next(err)
 		}
